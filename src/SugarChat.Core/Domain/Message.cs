283d07fe9e9d60dddba52630e3590a60043594b5
Brian <Brian@meikyo-corp.com>
2021-04-20 09:45:14 +0800

    add mongo and relative integration test

--- src/SugarChat.Core/Domain/Message.cs
+++ src/SugarChat.Core/Domain/Message.cs
@@ -10,5 +10,51 @@ namespace SugarChat.Core.Domain
         public Guid From { get; private set; }
         public Guid To { get; private set; }
         public MessageStatus Status { get; private set; }
+
+        public Message(Guid id, 
+            string content,
+            DateTime publishDateTime,
+            Guid from,
+            Guid to,
+            MessageStatus status
+            )
+        {
+            Id = CheckId(id);
+            Content = CheckContent(content);
+            PublishDateTime = CheckPublishDateTime(publishDateTime);
+            From = CheckFrom(from);
+            To = CheckTo(to);
+            Status = CheckStatus(status);
+        }
+
+        private MessageStatus CheckStatus(MessageStatus status)
+        {
+            return status;
+        }
+
+        private Guid CheckTo(Guid to)
+        {
+            return to;
+        }
+
+        private Guid CheckFrom(Guid from)
+        {
+            return from;
+        }
+
+        private DateTime CheckPublishDateTime(DateTime publishDateTime)
+        {
+            return publishDateTime;
+        }
+
+        private string CheckContent(string content)
+        {
+            return content;
+        }
+
+        private Guid CheckId(Guid id)
+        {
+            return id;
+        }
     }
 }
---
+++ test/SugarChat.IntegrationTest/TestBase.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Extensions.Configuration;
+
+namespace SugarChat.IntegrationTest
+{
+    public abstract class TestBase
+    {
+        protected readonly IConfiguration _configuration;
+        
+        protected TestBase()
+        {
+            _configuration = new ConfigurationBuilder().AddJsonFile("appsettings.json").Build();
+        }
+    }
+}

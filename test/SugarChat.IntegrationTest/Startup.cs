434156475b1caff6a12612dc3a9c26918076897c
fanslead <136590076@qq.com>
2021-04-25 12:13:18 +0800

    test project add DI

---
+++ test/SugarChat.IntegrationTest/Startup.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SugarChat.IntegrationTest
+{
+    public class Startup
+    {
+        public void ConfigureHost(IHostBuilder hostBuilder)
+        {
+            hostBuilder
+                .ConfigureAppConfiguration(builder =>
+                {
+                    // 注册配置
+                    builder
+                        .AddJsonFile("appsettings.json")
+                        ;
+                })
+                .ConfigureServices((context, services) =>
+                {
+                    // 注册自定义服务
+                })
+                ;
+        }
+        public void ConfigureServices(IServiceCollection services, HostBuilderContext hostBuilderContext)
+        {
+        }
+
+        // 可以添加要用到的方法参数，会自动从注册的服务中获取服务实例，类似于 asp.net core 里 Configure 方法
+        public void Configure(IServiceProvider applicationServices)
+        {
+            // 有一些测试数据要初始化可以放在这里
+        }
+    }
+}

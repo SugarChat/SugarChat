5f5e770476eb43edbaa61aa818781775a05f2450
BERTON.L <BERTON.L@DESKTOP-AJ4MN70>
2021-04-29 11:59:16 +0000

    add push demo

---
+++ src/SugarChat.Core/Services/Push/IPushService.cs
@@ -0,0 +1,14 @@
+﻿using Mediator.Net.Contracts;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace SugarChat.Core.Services.Push
+{
+    /// <summary>
+    /// use to notification or signalr
+    /// </summary>
+    public interface IPushService
+    {
+        Task Push(IEvent @event, CancellationToken cancellationToken);
+    }
+}

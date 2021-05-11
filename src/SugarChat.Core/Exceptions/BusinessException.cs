c966bf20f6a072c9af40fef5eb36babedbdec463
Brian <Brian@meikyo-corp.com>
2021-05-11 10:13:23 +0800

    add exceptions

--- src/SugarChat.Core/Exceptions/BusinessException.cs
+++ src/SugarChat.Core/Exceptions/BusinessException.cs
@@ -1,12 +1,20 @@
 ﻿using System;
+using Serilog.Events;
 
 namespace SugarChat.Core.Exceptions
 {
     public class BusinessException : Exception, IBusinessException
     {
-        public BusinessException(string message) : base(message)
+        public BusinessException(LogEventLevel logLevel, string message) : base(message)
         {
-            
+            LogLevel = logLevel;
         }
+        
+        public BusinessException(LogEventLevel logLevel, string message, Exception innerException) : base(message, innerException)
+        {
+            LogLevel = logLevel;
+        }
+
+        public LogEventLevel LogLevel { get; }
     }
 }

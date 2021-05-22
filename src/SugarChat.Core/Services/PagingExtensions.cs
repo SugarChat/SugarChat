88b50c7f0f02ffc06d6dc0cda81fb78538c5228d
Brian <Brian@meikyo-corp.com>
2021-05-22 16:28:26 +0800

    deal with null pageSettings

--- src/SugarChat.Core/Services/PagingExtensions.cs
+++ src/SugarChat.Core/Services/PagingExtensions.cs
@@ -10,14 +10,19 @@ namespace SugarChat.Core.Services
         {
             return collection.Skip(pageSettings.PageSize * (pageSettings.PageNum - 1)).Take(pageSettings.PageSize);
         }
-        
+
         public static IEnumerable<T> Paging<T>(this IEnumerable<T> collection, PageSettings pageSettings)
         {
             return collection.Skip(pageSettings.PageSize * (pageSettings.PageNum - 1)).Take(pageSettings.PageSize);
         }
-        
+
         public static IMongoQueryable<T> Paging<T>(this IMongoQueryable<T> collection, PageSettings pageSettings)
         {
+            if (pageSettings is null)
+            {
+                return collection;
+            }
+
             return collection.Skip(pageSettings.PageSize * (pageSettings.PageNum - 1)).Take(pageSettings.PageSize);
         }
     }

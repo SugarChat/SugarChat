741beaadacfa65dfb6b5c611a76268c11fefc097
fanslead <136590076@qq.com>
2021-05-28 11:46:59 +0800

    IServerClient

--- src/SugarChat.SignalR.ServerClient/IServerClient.cs
+++ src/SugarChat.SignalR.ServerClient/IServerClient.cs
@@ -1,10 +1,24 @@
-﻿using System;
+﻿using SugarChat.SignalR.Server.Models;
+using System;
 using System.Collections.Generic;
 using System.Text;
+using System.Threading.Tasks;
 
 namespace SugarChat.SignalR.ServerClient
 {
     public interface IServerClient
     {
+
+        Task<string> GetConnectionUrl(string userIdentifier);
+
+        Task Group(GroupActionModel model);
+
+        Task SendMessage(SendMessageModel model);
+
+        Task SendMassMessage(SendMassMessageModel model);
+
+        Task SendCustomMessage(SendCustomMessageModel model);
+
+        Task SendMassCustomMessage(SendMassCustomMessageModel model);
     }
 }

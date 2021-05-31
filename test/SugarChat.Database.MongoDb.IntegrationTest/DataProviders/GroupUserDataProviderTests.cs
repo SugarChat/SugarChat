aab843411363516b19efbd64a22e96f68779bdd7
Brian <Brian@meikyo-corp.com>
2021-05-31 14:08:20 +0800

    add GroupUserDataProviderTests

---
+++ test/SugarChat.Database.MongoDb.IntegrationTest/DataProviders/GroupUserDataProviderTests.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Linq;
+using System.Threading.Tasks;
+using Shouldly;
+using SugarChat.Core.Domain;
+using SugarChat.Core.Services.GroupUsers;
+using SugarChat.Shared.Paging;
+using Xunit;
+
+namespace SugarChat.Database.MongoDb.IntegrationTest.DataProviders
+{
+    public class GroupUserDataProviderTests : ServiceFixture
+    {
+        private readonly IGroupUserDataProvider _groupUserDataProvider;
+
+        public GroupUserDataProviderTests()
+        {
+            _groupUserDataProvider = new GroupUserDataProvider(Repository);
+        }
+        
+        
+    }
+}

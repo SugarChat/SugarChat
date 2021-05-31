a0f157daab4810f2e506d2e2007f9b94282990ce
Brian <Brian@meikyo-corp.com>
2021-05-31 15:51:06 +0800

    test

---
+++ test/SugarChat.Database.MongoDb.IntegrationTest/DatabaseFixture.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using Microsoft.Extensions.Configuration;
+using MongoDB.Driver;
+using SugarChat.Data.MongoDb;
+using SugarChat.Data.MongoDb.Settings;
+
+namespace SugarChat.Database.MongoDb.IntegrationTest
+{
+    public class DatabaseFixture : IDisposable
+    {
+        public MongoClient Client{ get;}
+        public MongoDbSettings Settings { get;}
+        public DatabaseFixture()
+        {
+            Settings = new MongoDbSettings();
+            IConfiguration configuration = new ConfigurationBuilder()
+                .AddJsonFile("appsettings.json")
+                .Build();
+            configuration.GetSection("MongoDb")
+                .Bind(Settings);
+            Client = new MongoClient(Settings.ConnectionString);
+        }
+
+        public void Dispose()
+        {
+        }
+    }
+}

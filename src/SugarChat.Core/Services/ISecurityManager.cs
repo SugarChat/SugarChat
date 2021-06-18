de3299c703af2ef544e85c4b780846c649832861
Jericho.Z <JERICHO.Z@TRINEE-MLA15>
2021-06-18 01:12:15 -0700

    SecurityManager

---
+++ src/SugarChat.Core/Services/ISecurityManager.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SugarChat.Core.Services
+{
+    public interface ISecurityManager : IService
+    {
+        Task<bool> IsSupperAdmin();
+    }
+
+    public class SecurityManager : ISecurityManager
+    {
+        public Task<bool> IsSupperAdmin()
+        {
+            return Task.FromResult(true);
+        }
+    }
+}

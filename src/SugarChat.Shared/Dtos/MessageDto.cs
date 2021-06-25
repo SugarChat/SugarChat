e8a6f03a971a887d8baee41302f04a18f0fff886
Jericho.Z <JERICHO.Z@TRINEE-MLA15>
2021-06-25 02:14:13 -0700

    MessageDto Id

--- src/SugarChat.Shared/Dtos/MessageDto.cs
+++ src/SugarChat.Shared/Dtos/MessageDto.cs
@@ -4,11 +4,12 @@ namespace SugarChat.Shared.Dtos
 {
     public class MessageDto
     {
+        public string Id { get; set; }
         public string GroupId { get; set; }
         public string Content { get; set; }
         public int Type { get; set; }
         public string SentBy { get; set; }
-        public DateTimeOffset SentTime { get; set; }       
+        public DateTimeOffset SentTime { get; set; }
         public bool IsSystem { get; set; }
         public object Payload { get; set; }
     }

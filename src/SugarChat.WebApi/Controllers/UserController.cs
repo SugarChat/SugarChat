0d55c540bfb3736117386c9a892a5995ce96669c
Jericho.Z <JERICHO.Z@TRINEE-MLA15>
2021-07-20 00:31:37 -0700

    build test

--- src/SugarChat.WebApi/Controllers/UserController.cs
+++ src/SugarChat.WebApi/Controllers/UserController.cs
@@ -83,7 +83,7 @@ namespace SugarChat.WebApi.Controllers
         [Route("batchAddUsers"), HttpPost]
         [ProducesResponseType(statusCode: StatusCodes.Status200OK, type: typeof(SugarChatResponse))]
         public async Task<IActionResult> BatchAddUsers(BatchAddUsersCommand command)
-        {
+        { 
             var response = await _mediator.SendAsync<BatchAddUsersCommand, SugarChatResponse>(command);
             return Ok(response);
         }

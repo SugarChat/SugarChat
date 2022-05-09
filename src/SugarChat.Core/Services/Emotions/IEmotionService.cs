c4166b7420816c605d85b1103368c86304aa1829
Brian <BrianCodeman@protonmail.com>
2022-05-09 09:56:36 +0800

    add block-scope of namespace to be compatible with .net5

--- src/SugarChat.Core/Services/Emotions/IEmotionService.cs
+++ src/SugarChat.Core/Services/Emotions/IEmotionService.cs
@@ -5,16 +5,17 @@ using SugarChat.Message.Commands.Emotions;
 using SugarChat.Message.Dtos.Emotions;
 using SugarChat.Message.Requests.Emotions;
 
-namespace SugarChat.Core.Services.Emotions;
-
-public interface IEmotionService : IService
+namespace SugarChat.Core.Services.Emotions
 {
-    Task<IEnumerable<EmotionDto>> GetEmotionByIdsAsync(GetEmotionByIdsRequest request,
-        CancellationToken cancellationToken = default);
+    public interface IEmotionService : IService
+    {
+        Task<IEnumerable<EmotionDto>> GetEmotionByIdsAsync(GetEmotionByIdsRequest request,
+            CancellationToken cancellationToken = default);
 
-    Task<IEnumerable<EmotionDto>> GetUserEmotionsAsync(GetUserEmotionsRequest request,
-        CancellationToken cancellationToken = default);
+        Task<IEnumerable<EmotionDto>> GetUserEmotionsAsync(GetUserEmotionsRequest request,
+            CancellationToken cancellationToken = default);
 
-    Task AddEmotionAsync(AddEmotionCommand command, CancellationToken cancellationToken = default);
-    Task RemoveEmotionAsync(RemoveEmotionCommand command, CancellationToken cancellationToken = default);
+        Task AddEmotionAsync(AddEmotionCommand command, CancellationToken cancellationToken = default);
+        Task RemoveEmotionAsync(RemoveEmotionCommand command, CancellationToken cancellationToken = default);
+    }
 }

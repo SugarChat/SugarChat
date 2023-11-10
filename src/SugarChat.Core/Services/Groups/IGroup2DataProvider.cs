9dc99c19bc1ffec0306bd076fcd2bf6689139a44
JERICHO.Z <547243012@qq.com>
2023-11-10 09:23:53 +0800

    change entity structure

--- src/SugarChat.Core/Services/Groups/IGroup2DataProvider.cs
+++ src/SugarChat.Core/Services/Groups/IGroup2DataProvider.cs
@@ -1,4 +1,5 @@
-﻿using SugarChat.Core.Domain;
+﻿using AutoMapper;
+using SugarChat.Core.Domain;
 using SugarChat.Core.IRepositories;
 using System.Threading;
 using System.Threading.Tasks;
@@ -9,6 +10,8 @@ namespace SugarChat.Core.Services.Groups
     {
         Task<Group2> GetByIdAsync(string id, CancellationToken cancellationToken = default);
 
+        Task<Group2> GetByIdAsync(Group group, CancellationToken cancellationToken = default);
+
         Task AddAsync(Group2 group, CancellationToken cancellationToken = default);
 
         Task UpdateAsync(Group2 group, CancellationToken cancellationToken = default);
@@ -17,16 +20,33 @@ namespace SugarChat.Core.Services.Groups
     public class Group2DataProvider : IGroup2DataProvider
     {
         private readonly IRepository _repository;
+        private readonly IMapper _mapper;
 
-        public Group2DataProvider(IRepository repository)
+        public Group2DataProvider(IRepository repository, IMapper mapper)
         {
             _repository = repository;
+            _mapper = mapper;
         }
 
         public async Task<Group2> GetByIdAsync(string id, CancellationToken cancellationToken = default)
         {
             return await _repository.SingleOrDefaultAsync<Group2>(x => x.Id == id, cancellationToken)
                 .ConfigureAwait(false);
+        }
+
+        public async Task<Group2> GetByIdAsync(Group group, CancellationToken cancellationToken = default)
+        {
+            var group2 = await _repository.SingleOrDefaultAsync<Group2>(x => x.Id == group.Id, cancellationToken)
+                .ConfigureAwait(false);
+            if (group2 != null)
+            {
+                return group2;
+            }
+            else
+            {
+                await _repository.AddAsync(_mapper.Map<Group2>(group), cancellationToken);
+                return _mapper.Map<Group2>(group);
+            }
         }
 
         public async Task AddAsync(Group2 group, CancellationToken cancellationToken = default)
